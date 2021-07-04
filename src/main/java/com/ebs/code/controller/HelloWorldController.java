1d267202cebaabce08ca0528eb49370abef82bd7
SarojKanta Maharana <sarojkantaa.maharana@gmail.com>
2021-07-04 12:40:54 +0530

    Update HelloWorldController.java

--- src/main/java/com/ebs/code/controller/HelloWorldController.java
+++ src/main/java/com/ebs/code/controller/HelloWorldController.java
@@ -10,6 +10,6 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "/hello")
 	public String sayHelloWorld() {
-		return "Hello Saroj, How are you? I am good.";
+		return "Hello Saroj, How are you? ";
 	}
 }
